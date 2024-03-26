04aa45e19a060f84f6860a1d283b45c6bccf5938
sabrinara <saashid18@gmail.com>
2024-03-27 00:55:31 +0600

    app.js initial

--- Backend-Mongoose/src/app.js
+++ Backend-Mongoose/src/app.js
@@ -5,24 +5,25 @@ const port = process.env.PORT || 5000;
 
 app.get("/health", (req, res) => {
     res.send("Backend Server is Running");
-  });
-  
-  //for Error Handling ,if URL is Invalid 
-  app.all("*", (req, res, next) => {
+});
+
+//for Error Handling ,if URL is Invalid 
+//error is here in the next variable ,in the below 4 lines codes the middleware will handle it
+app.all("*", (req, res, next) => {
     const error = new Error(`The Requested URL is Invalid: [${req.url}]`);
     error.status = 404;
     next(error);
-  });
-  
-  //for Error Handling if any Error is thrown by any middleware
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-      message: err.message,
-    });
-  });
-  
+});
 
-    app.listen(port, (req, res) => {
-      console.log(`Backend  Server running On Port: ${port}`);
+//for Error Handling if any Error is thrown by any middleware
+//for error handling it is a global middleware
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
     });
-  
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Backend  Server running On Port: ${port}`);
+});
+
