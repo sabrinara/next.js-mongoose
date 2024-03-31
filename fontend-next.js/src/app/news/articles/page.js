534743c754a26aa61567b9a8f92af728355d5667
sabrinara <saashid18@gmail.com>
2024-03-31 23:41:47 +0600

    nested route news component

---
+++ fontend-next.js/src/app/news/articles/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArticlePage = () => {
+    return (
+        <div>
+            <h1>Article Page (nested route)</h1>
+        </div>
+    );
+};
+
+export default ArticlePage;
